1e7c1b59d67e0947c1ebf1e34314c2d6442f567b
root <root@SCOOBY.localdomain>
2017-03-18 18:01:16 -0400

    Massive update, implemented AES and DES. Partial writeup has been included

--- joint.shapes.logic.js
+++ joint.shapes.logic.js
@@ -5,6 +5,23 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+ Array.prototype.rotate = (function() {
+    // save references to array functions to make lookup faster
+    var push = Array.prototype.push,
+        splice = Array.prototype.splice;
+
+    return function(count) {
+        var len = this.length >>> 0, // convert to uint
+            count = count >> 0; // convert to int
+
+        // convert count to value in range [0, len)
+        count = ((count % len) + len) % len;
+
+        // use splice.call() instead of this.splice() to make function generic
+        push.apply(this, splice.call(this, 0, count));
+        return this;
+    };
+})(); //Circular rotate method for arrays
 
 joint.shapes.logic = {};
 	
@@ -235,11 +252,164 @@ joint.shapes.logic.Fiestel = joint.shapes.logic.Gate21.extend({
     }, joint.shapes.logic.Gate21.prototype.defaults),
 
     operation: function(input1, input2) {
-		return input1 ^ input2;
+			//S-boxes
+	S = [ 
+	[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+	0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+	4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+	15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+	[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+	3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+	0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+	13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+	[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+	13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+	13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+	1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+	[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+	13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+	10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+	3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+	[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+	14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+	4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+	11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+	[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+	10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+	9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+	4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+	[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+	13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+	1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+	6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+	[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+	1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+	7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+	2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+	];
+	P = [15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9, 1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24];
+		if(input1 && input1[0] && input2 && input2[0]){
+			var key = input1[0];
+			//console.log("key: " + key);
+			var rightbitstring = parseInt(input2[0],10).toString(2);
+			//console.log("rightbitstring: " + rightbitstring);
+			var rightbits = [];
+			for(i = 0; i < 32; i++){
+				if(i >= rightbitstring.length){
+					rightbits.unshift(0);
+				}
+				else{
+					rightbits[i] = rightbitstring[i];
+				}
+			}
+			//console.log("rightbits: " + rightbits);
+			var E = [31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24, 23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0];
+			var expandrightbits = [];
+			for(i = 0; i < E.length; i++){
+				expandrightbits[i] = rightbits[E[i]];
+			}
+			//console.log("expandrightbits: " + expandrightbits);
+			var xorbits = [];
+			for(i = 0; i < expandrightbits.length; i++){
+				xorbits[i] = expandrightbits[i] ^ key[i];
+			}
+			//console.log("xorbits: " + xorbits);
+			var outnibs = [];
+			for(i = 0; i < 8; i++){
+				//outnibs[i] = S[i][xorbits[6*i + 4] + 2*xorbits[6*i + 3] + 4*xorbits[6*i + 2] + 8*xorbits[6*i + 1] + 32*xorbits[6*i]];
+				outnibs[i] = S[i][xorbits[6*i + 4] + 2*xorbits[6*i + 3] + 4*xorbits[6*i + 2] + 8*xorbits[6*i + 1] +16*xorbits[6*i + 5]+ 32*xorbits[6*i]];
+				//out *= 16;
+				//out += outnibs[i];
+			}
+			//console.log("outnibs: " + outnibs);
+			var outbits = [];
+			for(i = 0; i < 8; i++){
+				var bits = outnibs[i].toString(2);
+				//if bits.length = 4 then no bits should be set to zero
+				//if bits.length = 3 then the first bit should be set to zero
+				for(j = 0; j < 4; j++){
+					if(bits.length + j < 4){
+						outbits[4*i + j] = 0;
+					}
+					else{
+						outbits[4*i + j] = bits[j + bits.length - 4];
+					}
+				}
+			}
+			//console.log("outbits: " + outbits);
+			var out = 0;
+			for(i = 0; i < 32; i++){
+				out *= 2;
+				out += parseInt(outbits[P[i]], 10); //Apply final permutation and compute number
+			}
+			return out;
+		}
+		return 42;
         //return input1 ^ input2;
 		//return (input1 << 1) + (input1 >> 31); //Left cyclic shift, 32-bit blocksize
 		//return (input1 >> 1) + (input1 << 31); //Right cyclic shift, 32-bit blocksize
 
+    }
+
+});
+
+joint.shapes.logic.AESSbox = joint.shapes.logic.Gate11.extend({
+
+    defaults: _.defaultsDeep({
+
+        type: 'logic.AESSbox',
+        attrs: { image: { 'xlink:href': 'sbox.png'}}
+
+    }, joint.shapes.logic.Gate11.prototype.defaults),
+
+    operation: function(input) {
+        return input;
+    }
+
+});
+
+joint.shapes.logic.AESShiftRows = joint.shapes.logic.Gate11.extend({
+
+    defaults: _.defaultsDeep({
+
+        type: 'logic.AESShiftRows',
+        attrs: { image: { 'xlink:href': 'shiftrows.png'}}
+
+    }, joint.shapes.logic.Gate11.prototype.defaults),
+
+    operation: function(input) {
+        return input;
+    }
+
+});
+
+joint.shapes.logic.AESMixColumns = joint.shapes.logic.Gate11.extend({
+
+    defaults: _.defaultsDeep({
+
+        type: 'logic.AESMixColumns',
+        attrs: { image: { 'xlink:href': 'mixcolumns.png'}}
+
+    }, joint.shapes.logic.Gate11.prototype.defaults),
+
+    operation: function(input) {
+        return input;
+    }
+
+});
+joint.shapes.logic.AESXor = joint.shapes.logic.Gate21.extend({
+
+    defaults: _.defaultsDeep({
+
+        type: 'logic.AESXor',
+        attrs: { image: { 'xlink:href': 'AESxor.png'}}
+    }, joint.shapes.logic.Gate21.prototype.defaults),
+
+    operation: function(input1, input2) {
+		return input1;
+        //return input1 ^ input2;
+		//return (input1 << 1) + (input1 >> 31); //Left cyclic shift, 32-bit blocksize
+		//return (input1 >> 1) + (input1 << 31); //Right cyclic shift, 32-bit blocksize
     }
 
 });
@@ -314,7 +484,8 @@ joint.shapes.logic.Xor = joint.shapes.logic.Gate21.extend({
     }, joint.shapes.logic.Gate21.prototype.defaults),
 
     operation: function(input1, input2) {
-        return (!input1 || input2) && (input1 || !input2);
+        //return (!input1 || input2) && (input1 || !input2);
+        return input1 ^ input2; 
     }
 
 });
